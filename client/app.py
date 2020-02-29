c559ccd08b22e8a2291d9601111bcddf749bc61e
Paulo Pessoa <pvictorpessoa@hotmail.com>
2020-02-29 20:55:36 -0300

    fix pep8

--- client/app.py
+++ client/app.py
@@ -6,12 +6,12 @@ from functools import reduce
 
 def lambda_handler(event, context):
     message = receive_message()
-    body = json.loads(message.body)
-    function = body.get('function')
-    args = body.get('args')
+
+    function, args = get_data(message)
 
     _operator = get_function(function)
     result = reduce(_operator, args)
+
     message.delete()
     return {
         "statusCode": 200,
@@ -23,6 +23,25 @@ def lambda_handler(event, context):
     }
 
 
+def receive_message():
+    queue = get_queue()
+    return queue.receive_messages(
+        MaxNumberOfMessages=1
+        )[0]
+
+
+def get_queue():
+    sqs = boto3.resource('sqs')
+    return sqs.get_queue_by_name(QueueName='newton_sqs.fifo')
+
+
+def get_data(message):
+    body = json.loads(message.body)
+    function = body.get('function')
+    args = body.get('args')
+    return function, args
+
+
 def get_function(function_name):
     _functions = {
         'sum': operator.add,
@@ -31,15 +50,3 @@ def get_function(function_name):
         'multiply': operator.mul,
     }
     return _functions[function_name]
-
-
-def get_queue():
-    sqs = boto3.resource('sqs')
-    return sqs.get_queue_by_name(QueueName='newton_sqs.fifo')
-
-
-def receive_message():
-    queue = get_queue()
-    return queue.receive_messages(
-        MaxNumberOfMessages=1
-        )[0]
